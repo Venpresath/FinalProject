8f133e83abc6446b034129518137c978383f8961
unknown <kelly.achino@gmail.com>
2018-06-15 14:22:03 -0400

    update

--- service.js
+++ service.js
@@ -8,7 +8,7 @@
         let getAPI = function(){
             let url = `https://api.musixmatch.com/ws/1.1/artist.search?format=jsonp&callback=callback&q_artist=kanye&apikey=${APIKey}`;
             return $http.get(url).then(function(response) {  
-            console.log(response);
+            console.log(response.data);
         
         })
     }
