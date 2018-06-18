711b8812eb2721c57d2ad0023b70522e24cfb9dd
DJ <daymonjohnson75@yahoo.com>
2018-06-18 14:58:18 -0400

    init parameters

--- service.js
+++ service.js
@@ -4,7 +4,6 @@
         let vm = this;
         const APIKey = "c42ef466fff57d1c817a1efd2f2ebf38";
 
-
         let getAPI = function () {
             let url = `https://cors-anywhere.herokuapp.com/api.musixmatch.com/ws/1.1/artist.search?format=json&q_artist=kanye&apikey=${APIKey}`;
             return $http.get(url).then(function (response) {
