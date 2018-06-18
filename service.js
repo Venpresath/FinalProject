38e37144fcaa35e90df6e045491bb01160e564e5
clairose <clairose@umich.edu>
2018-06-18 10:16:23 -0400

    Tried to get lyrics to show up

--- service.js
+++ service.js
@@ -9,16 +9,23 @@
             let url = `https://cors-anywhere.herokuapp.com/api.musixmatch.com/ws/1.1/artist.search?format=json&q_artist=kanye&apikey=${APIKey}`;
             return $http.get(url).then(function(response) {  
             console.log(response.data.message.body.artist_list[0].artist.artist_name);
-        
-        })
-    }
+            return response.data.message.body.artist_list[0].artist.artist_name;
+        });
+        let getLyrics = function(artist){
+            let url = `https://cors-anywhere.herokuapp.com/api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=callback&q_artist=${artist}&apikey=${APIKey}`;
+            return $http.get(url).then(function(response) {  
+            console.log(response.data.message.body.lyrics.lyrics_body);
+        });
+    
         
 return {
-    getAPI
+    getAPI,
+    getLyrics
 }
-
+        }
     }
     angular
         .module("app")
         .factory("service", service);
+}
 }
