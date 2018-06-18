8c5ee928c6e3df6bf2655346ea488dde3acfdfec
Roger Winright <venpresath@live.com>
2018-06-18 09:13:09 -0400

    stored commit

--- service.js
+++ service.js
@@ -5,22 +5,24 @@
         const APIKey = "e89505a720549b7b6b4a37d28142e420";
 
 
-        let callAPI = function(){
-            let url = `https://api.musixmatch.com/ws/1.1/artist.search?format=json?q_artist=kanye&apikey=${APIKey}`;
-            // $http.jsonp(url, {jsonpCallbackParam: 'callback'})
-            return $http.get(url).then(function(response) {  
-            // let sampleArtist = response.data.message.body.artist_list[0].artist.artist_name;
-            console.log(response.data);
-            // return sampleArtist;
-        });
-    }
-        
-return {
-    callAPI
-}
 
+
+
+        let callAPI = function () {
+
+            let url = `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/artist.search?format=jsonp&callback=callback&q_artist=kanye&apikey=${APIKey}`;
+            
+            return $http.get(url).then(function(response) {  
+                console.log(response.data);
+            });
     }
-    angular
-        .module("app")
-        .factory("service", service);
+
+    return {
+        callAPI
+    }
+
+}
+angular
+    .module("app")
+    .factory("service", service);
 }
