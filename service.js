be8ddfeff0f9687f6d6b236c82df4a28effe65b6
clairose <clairose@umich.edu>
2018-06-19 16:12:26 -0400

    Only popular songs will show up

--- service.js
+++ service.js
@@ -18,7 +18,6 @@
         let getLyrics = function (trackId) {
                 let url = `https://cors-anywhere.herokuapp.com/api.musixmatch.com/ws/1.1/track.snippet.get?format=json&track_id=${trackId}&apikey=f4e510d4d5b6f478a5d43f2bbc92a036`
                 return $http.get(url).then(function (response) {
-                    console.log(url);
                     let lyrics = response.data.message.body.snippet.snippet_body;
                     console.log(lyrics);
                     setLyrics(lyrics);
@@ -28,7 +27,7 @@
         
             let getTrackId = function (artist) {
                 var artist = artist.split(" ").join("%20");
-                let url = `https://cors-anywhere.herokuapp.com/api.musixmatch.com/ws/1.1/track.search?format=json&q_artist=${artist}&apikey=c42ef466fff57d1c817a1efd2f2ebf38`
+                let url = `https://cors-anywhere.herokuapp.com/api.musixmatch.com/ws/1.1/track.search?format=json&q_artist=${artist}&s_track_rating=desc&apikey=c42ef466fff57d1c817a1efd2f2ebf38`
                 return $http.get(url).then(function(response){
                     console.log(response.data.message.body.track_list[0].track.artist_name);
                     let trackNum = response.data.message.body.track_list[0].track.track_id;
