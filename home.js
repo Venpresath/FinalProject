1ce65327b0de621fa4473c8ca6008b01dac5e5f7
unknown <kelly.achino@gmail.com>
2018-06-20 15:19:17 -0400

    wins and losses being tracked

--- home.js
+++ home.js
@@ -7,7 +7,8 @@
         <p>Lyric: {{$ctrl.lyrics}}</p>
         <p> ID: {{$ctrl.songNum}}</p>
         <p> Song Name: {{$ctrl.songName}}</p>
-        <input type = "text" placeholder="Guess the song" ng-model="$ctrl.guess"><button ng-click="$ctrl.getSongName($ctrl.songNum)">GEET'EM</button>
+        <input type = "text" placeholder="Guess the song" ng-model="$ctrl.guess"><button ng-click="$ctrl.getSongName($ctrl.songNum); $ctrl.getTrackId($ctrl.songNum)">GEET'EM</button>
+        <p> Result: {{$ctrl.result}} {{$ctrl.condition}}</p>
         </div>`,
 // empty strings are created here that are filled in when a specific artist, lyric, guess is called. 
         controller: function (service) {
@@ -15,13 +16,36 @@
             vm.artist = "";
             vm.lyrics = "";
             vm.guess = "";
+            vm.result = "";
+            vm.wins = 0;
+            vm.losses = 0;
+            vm.condition = "";
 // guessSong function will determine if the users answer is correct or not and give an appropriate response
             vm.guessSong = function (guess) {
                 if (guess.toLowerCase() == vm.songName.toLowerCase()) {
                     console.log("correct");
+                    vm.result = "correct";
+                    vm.wins++;
+                    if (vm.wins === 3){
+                        vm.condition = "you win!";
+                        vm.wins = 0;
+                        vm.losses = 0;                       
+                    }
+                    
+                    return vm.result;
                 } else {
                     console.log("guess again");
-                }
+                    vm.result = "guess again";
+                    vm.losses++;
+                        if (vm.losses === 3){
+                            vm.condition = "you lose!";
+                            // if user lose count reaches 3 reset wins and losses 0. Reset start again. 
+                            vm.wins = 0;
+                            vm.losses = 0;
+                            
+                        }
+                    return vm.result;
+                } 
             }
 // getLyrics using the trackId to get lyrics
             vm.getLyrics = function () {
